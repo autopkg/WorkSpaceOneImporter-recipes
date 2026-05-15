c1c7bb3aa8cf4afa81019ac5d11fb72f5e8bf223
codeskipper <12513894+codeskipper@users.noreply.github.com>
2026-05-15 16:39:46 +0200

    fixing try-catch for ws1_app_versions_to_keep input validation

--- ws1-processors/WorkSpaceOnePruner.py
+++ ws1-processors/WorkSpaceOnePruner.py
@@ -155,7 +155,8 @@ class WorkSpaceOnePruner(WorkSpaceOneImporterBase):
         keep_versions_str = self.env.get("ws1_app_versions_to_keep")
         try:
             keep_versions = extract_first_integer_from_string(keep_versions_str)
-            self.output(f"ws1_app_versions_to_keep is set to: {keep_versions}", verbose_level=2)
+            if keep_versions is None:
+                raise ValueError("No integer found in ws1_app_versions_to_keep")
         except ValueError:
             self.output(
                 f"ws1_app_versions_to_keep setting {keep_versions_str} is out of range, "
